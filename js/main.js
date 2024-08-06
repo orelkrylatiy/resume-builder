ced77d354587349855dd1df29b2f3b26d6feeaeb
Maxim Agafonov <maxim700a@gmail.com>
2024-08-06 23:13:09 +0500

    feat: update files

--- js/main.js
+++ js/main.js
@@ -1,24 +1,26 @@
-import '../css/style.css'
-import javascriptLogo from '../javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+// Это предполагает, что вы установили jsPDF и html2canvas через npm
+import { jsPDF } from "jspdf";
+import html2canvas from "html2canvas";
+const { jsPDF } = window.jspdf;
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+document.addEventListener("DOMContentLoaded", () => {
+  const downloadBtn = document.getElementById("download-pdf");
+  if (downloadBtn) {
+    downloadBtn.addEventListener("click", downloadPDF);
+  }
+});
 
-setupCounter(document.querySelector('#counter'))
+function downloadPDF() {
+  html2canvas(document.body).then((canvas) => {
+    const imgData = canvas.toDataURL('image/png');
+    const pdf = new jsPDF({
+      orientation: "portrait",
+      unit: "px",
+      format: [canvas.width, canvas.height]
+    });
+    pdf.addImage(imgData, 'PNG', 0, 0, canvas.width, canvas.height);
+    pdf.save("downloaded-page.pdf");
+  }).catch(err => {
+    console.error("Ошибка при создании PDF: ", err);
+  });
+}
